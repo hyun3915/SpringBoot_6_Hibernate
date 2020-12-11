16a7197420ada489a31c6ee36dcd58754084dadf
hyun3915 <soonsoon123@naver.com>
2020-12-11 09:49:40 +0900

    Initial commit

---
+++ src/main/java/com/sub/sb6/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sub.sb6;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBoot6HibernateApplication.class);
+	}
+
+}
---
+++ src/main/java/com/sub/sb6/SpringBoot6HibernateApplication.java
@@ -0,0 +1,13 @@
+package com.sub.sb6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot6HibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot6HibernateApplication.class, args);
+	}
+
+}
